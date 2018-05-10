b36a857409fc9c0569f86e011c2f9f1a43f7a15a
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2018-05-10 06:32:07 +0000

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
+import {IndexRoute} from 'react-dom';
 import NavBar from '../components/NavBar';
 import Home from '../components/Home';
 import Actors from '../components/Actors';
@@ -13,9 +14,17 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      <div>
+        <NavBar/>
+        <Route path="/" component={App} >
+          <IndexRoute component={Home} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/actors" component={Actors} />
+          <Route path="/directors" component={Directors} />
+        </Route>
+      </div>
     </Router>
   );
 };
 
-export default App
+export default App;
